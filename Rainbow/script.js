21eadc16c7eb5a15f4f7825ad517d393439e72d1
Kenny530 <kenneth_garcia@davincischools.org>
2017-11-17 14:27:55 -0800

    Add files via upload

---
+++ Rainbow/script.js
@@ -0,0 +1,12 @@
+function makeChange(e){
+  e.classList.toggle("mine");
+}
+function froSty(e){
+  e.classList.toggle("bear");
+}
+function blaCk(e){
+  e.classList.toggle("black");
+}
+function pelLet(e){
+  e.classList.toggle("pel");
+}
