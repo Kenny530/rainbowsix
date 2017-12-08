6857ed9dedb7c87386aa424b0020b25c0d2d4989
Kenny530 <kenneth_garcia@davincischools.org>
2017-12-08 15:44:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 function makeChange(e){
   e.classList.toggle("mine");
+  e.classList.toggle("image");
 }
 function froSty(e){
   e.classList.toggle("bear");
+  e.classList.toggle("image");
 }
 function blaCk(e){
   e.classList.toggle("black");
+  e.classList.toggle("image");
 }
 function pelLet(e){
   e.classList.toggle("pel");
+  e.classList.toggle("image");
+}
+function detAil(e){
+  e.classList.toggle("snipe");
+  e.classList.toggle("image");
+}
+function droNe(e){
+  e.classList.toggle("shock");
+  e.classList.toggle("image");
+}
+function comMand(e){
+  e.classList.toggle("iqg");
+  e.classList.toggle("image");
+}
+function disAble(e){
+  e.classList.toggle("light");
+  e.classList.toggle("image");
+}
+function scaN(e){
+  e.classList.toggle("spain");
+  e.classList.toggle("image");
 }
